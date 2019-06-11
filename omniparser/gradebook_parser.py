545fa79cec3e2417e0ad915718e059f6287ce153
Rich Bubbico <rjb2182@stern.nyu.edu>
2019-06-10 23:05:02 -0400

    needs many fixes

--- omniparser/gradebook_parser.py
+++ omniparser/gradebook_parser.py
@@ -2,8 +2,16 @@ import pandas
 import os
  
 
-def calculate_average_grade_from_csv(my_csv_filepath):
-    df = pandas.read_csv(my_csv_filepath)
+#def calculate_average_grade_from_csv(my_csv_filepath):
+df = pandas.read_csv('/Users/richiebubbs/Downloads/GitHub/omniparser-starter-py/data/gradebook_2019.csv')
+rows = df.to_dict()
+
+grades = [r["final_grade"] for r in rows]
+avg_grades = sum(grades)/len(grades)
+print(avg_grades)
+
+
+#calculate_average_grade_from_csv(my_csv_filepath)
 
 
 
