2247cfa693acc1607af067dfb54304bdd3830757
Rich Bubbico <rjb2182@stern.nyu.edu>
2019-06-11 09:46:15 -0400

    invoking the function

--- omniparser/gradebook_parser.py
+++ omniparser/gradebook_parser.py
@@ -1,15 +1,22 @@
 import pandas
 import os
+import statistics
  
 
-#def calculate_average_grade_from_csv(my_csv_filepath):
-df = pandas.read_csv('/Users/richiebubbs/Downloads/GitHub/omniparser-starter-py/data/gradebook_2019.csv')
-rows = df.to_dict()
+def calculate_average_grade_from_csv(my_csv_filepath):
+    df = pandas.read_csv(my_csv_filepath)
+    rows = df.to_dict("records")
 
-grades = [r["final_grade"] for r in rows]
-avg_grades = sum(grades)/len(grades)
-print(avg_grades)
+    grades = [r["final_grade"] for r in rows]
+    avg_grade = statistics.mean(grades)
 
+    print(avg_grade)
+
+df =pandas.read_csv("/Users/richiebubbs/Downloads/GitHub/omniparser-starter-py/data/gradebook_2018.csv")
+
+calculate_average_grade_from_csv("/Users/richiebubbs/Downloads/GitHub/omniparser-starter-py/data/gradebook_2018.csv")
+
+breakpoint()
 
 #calculate_average_grade_from_csv(my_csv_filepath)
 
